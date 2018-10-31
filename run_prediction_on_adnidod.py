0862dd9786dbb6d2bae32b9933b17a65f26e7df5
KamalakerDadi <dkamalakarreddy@gmail.com>
2018-10-31 23:45:49 +0100

    script for adnidod timeseries

--- run_prediction_on_adnidod.py
+++ run_prediction_on_adnidod.py
@@ -1,13 +1,12 @@
-"""Script which starts from timeseries extracted on ADNI datasets. The
-   timeseries are pre-extracted using several atlases AAL, Harvard Oxford,
-   BASC, Power, MODL on these datasets and can be downloaded from
-   "https://osf.io/xhrcs/download"
+"""Script which starts from timeseries extracted on ADNIDOD. The timeseries
+   are pre-extracted using several atlases AAL, Harvard Oxford, BASC, Power,
+   MODL on these datasets and can be downloaded from
+   "https://osf.io/5aeny/download"
 
    Diagnostic information we used for prediction task is labelled as
-   "DX_Group". This information should be accessed from
-   http://adni.loni.usc.edu/data-samples/access-data/. The column name
-   "DX_Group" contains subjects AD and MCI which we used for our prediction
-   task.
+   "diagnosis" discriminating ptsd (post-traumatic stress disorder) and normal
+   controls. This information should be accessed from US Department of De-
+   fense (DoD) from http://adni.loni.usc.edu/data-samples/access-data/.
 
    After downloading, each folder should appear with name of the atlas and
    sub-folders, if necessary. For example, using BASC atlas, we have extracted
@@ -17,11 +16,11 @@
    dimensions 64 and 128 components.
 
    Dimensions of each atlas:
-        AAL - 116
-        BASC - 122
-        Power - 264
-        Harvard Oxford (cortical and sub-cortical) - 118
-        MODL - 64 and 128
+       AAL - 116
+       BASC - 122
+       Power - 264
+       Harvard Oxford (cortical and sub-cortical) - 118
+       MODL - 64 and 128
 
    The timeseries extraction process was done using Nilearn
    (http://nilearn.github.io/).
@@ -38,22 +37,25 @@ def _get_paths(phenotypic, atlas, timeseries_dir):
     """
     """
     timeseries = []
-    IDs_image = []
+    IDs_scan = []
     diagnosis = []
-    image_ids = phenotypic['Image_ID']
-    for index, image_id in enumerate(image_ids):
+    groups = []
+    scan_ids = phenotypic['ID_scan']
+    for index, scan_id in enumerate(scan_ids):
+        this_pheno = phenotypic[phenotypic['ID_scan'] == scan_id]
         this_timeseries = join(timeseries_dir, atlas,
-                               str(image_id) + '_timeseries.txt')
+                               scan_id + '_timeseries.txt')
         if os.path.exists(this_timeseries):
             timeseries.append(np.loadtxt(this_timeseries))
-            IDs_image.append(image_id)
-            diagnosis.append(phenotypic['DX_Group'][index])
-    return timeseries, diagnosis, IDs_image
+            IDs_scan.append(scan_id)
+            diagnosis.append(this_pheno['diagnosis'].values[0])
+            groups.append(this_pheno['ID_subject'].values[0])
+    return timeseries, diagnosis, groups, IDs_scan
 
 
 # Paths
-timeseries_dir = '/path/to/timeseries/directory/ADNI'
-predictions_dir = '/path/to/save/prediction/results/ADNI'
+timeseries_dir = '/path/to/timeseries/directory/ADNIDOD'
+predictions_dir = '/path/to/save/prediction/results/ADNIDOD'
 
 atlases = ['AAL', 'HarvardOxford', 'BASC/networks', 'BASC/regions',
            'Power', 'MODL/64', 'MODL/128']
@@ -74,7 +76,7 @@ for column_name in columns:
     results.setdefault(column_name, [])
 
 # phenotypes
-pheno_dir = '/path/to/csvfile/ADNI/description_file.csv'
+pheno_dir = '/path/to/csvfile/ADNIDOD/adnidod_demographic.csv'
 phenotypic = pd.read_csv(pheno_dir)
 
 # Connectomes per measure
@@ -91,7 +93,8 @@ cv = StratifiedShuffleSplit(n_splits=100, test_size=0.25,
 
 for atlas in atlases:
     print("Running predictions: with atlas: {0}".format(atlas))
-    timeseries, diagnosis, ids = _get_paths(phenotypic, atlas, timeseries_dir)
+    timeseries, diagnosis, groups, _ = _get_paths(phenotypic, atlas,
+                                                  timeseries_dir)
 
     _, classes = np.unique(diagnosis, return_inverse=True)
     iter_for_prediction = cv.split(timeseries, classes)
@@ -108,13 +111,13 @@ for atlas in atlases:
                 print('Supervised learning: classification {0}'.format(est_key))
                 estimator = sklearn_classifiers[est_key]
                 score = cross_val_score(estimator, conn_coefs,
-                                        classes, scoring='roc_auc',
+                                        classes, groups=groups, scoring='roc_auc',
                                         cv=[(train_index, test_index)])
                 results['atlas'].append(atlas)
                 results['iter_shuffle_split'].append(index)
                 results['measure'].append(measure)
                 results['classifier'].append(est_key)
-                results['dataset'].append('ADNI')
+                results['dataset'].append('ADNIDOD')
                 results['dimensionality'].append(dimensions[atlas])
                 results['scores'].append(score)
                 results['covariance_estimator'].append('LedoitWolf')
@@ -122,4 +125,4 @@ for atlas in atlases:
     # scores per atlas
     res.to_csv(join(predictions_dir, atlas, 'scores_{0}.csv'.format(atlas)))
 all_results = pd.DataFrame(results)
-all_results.to_csv('predictions_on_adni.csv')
+all_results.to_csv('predictions_on_adnidod.csv')
